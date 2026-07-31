ed67944d1c72775a157260d78e608ca7a5dcb07a
MarianoAnTar <marianoantar@gmail.com>
2026-07-31 11:07:56 -0300

    Agrega el modelo 'Juego' con campos para nombre, plataforma, precio e imagen.

--- tienda_videojuegos/catalogo/models.py
+++ tienda_videojuegos/catalogo/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Juego(models.Model):
+    nombre = models.CharField(max_length=100)
+    plataforma = models.CharField(max_length=50)
+    precio = models.DecimalField(max_digits=6, decimal_places=2)
+    # imagen_url = models.URLField()
+    imagen_url = models.CharField(max_length=200, default="default.png")
+
+    def __str__(self):
+        return self.nombre
+    
